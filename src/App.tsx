8f1e781707fc17f57094ccd2112df69011f54443
zanllp <zanllp@163.com>
2020-12-10 02:18:01 +0800

    chore: 删除多余符号

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default () => (
   <div>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
